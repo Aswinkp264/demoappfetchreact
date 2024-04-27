fc2272ead360ea41b1f9474daaad238dea1cbaf2
aswinkp <kpaswin264@gmail.com>
2024-04-27 11:58:47 +0530

    update

--- src/components/fetches/Fetches.js
+++ src/components/fetches/Fetches.js
@@ -11,17 +11,21 @@ const ProductList = () => {
   }, []); // Empty dependency array to only run this effect once on component mount
 
   return (
-    <div className="product-list">
-      {products.map(product => (
-        <div key={product.id} className="card">
-          <img src={product.image} alt={product.title} />
-          <div className="card-body">
-            <h5 className="card-title">{product.title}</h5>
-            <p className="card-price">${product.price}</p>
-            <p className="card-description">{product.description}</p>
+    <div className="container">
+      <div className="row">
+        {products.map(product => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <div className="card h-100">
+              <img src={product.image} className="card-img-top" alt={product.title} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-price">${product.price}</p>
+                <p className="card-description">{product.description}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
